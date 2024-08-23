bcb44749625d877889e0a5db73e506355a083cee
MuriloC2 <murilo.ti100@gmail.com>
2024-08-22 21:41:24 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import mongoose from "mongoose";
+
+import User from "./models/User.js";
+
+const app = express();
+
+app.use(express.json());
+
+const users = [];
+
+app.get("/users", async (request, response) => {
+  const users = await User.find();
+
+  return response.status(200).json(users);
+});
+
+app.post("/users", async (request, response) => {
+  const user = request.body;
+
+  const newUser = await User.create(user);
+
+  return response.status(201).json(newUser);
+});
+
+mongoose
+  .connect(
+    
+  )
+  .then(() => console.log("Database connected."))
+  .catch(() => console.log("Error connecting database"));
+
+app.listen(3000);
